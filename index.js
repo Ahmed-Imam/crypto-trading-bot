91b8502bfde7adc70094c3e1ebef727ad3cd62fc
ahmed-imam <ahmedimam49@gmail.com>
2021-08-19 23:24:05 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser')
+const http = require('http')
+const Web3 = require('web3')
+const HDWalletProvider = require('@truffle/hdwallet-provider')
+const moment = require('moment-timezone')
+const numeral = require('numeral')
+const _ = require('lodash')
+const axios = require('axios')
+const { UNISWAP_FACTORY_ABI, UNISWAP_FACTORY_ADDRESS, UNISWAP_EXCHANGE_ABI, KYBER_RATE_ABI, KYBER_RATE_ADDRESS }  =  require('./src/exchanges') 
+const { tokens } = require('./src/tokens')
+// SERVER CONFIG
+const PORT = process.env.PORT || 5000
+const app = express();
+const server = http.createServer(app).listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+// WEB3 CONFIG
+const web3 = new Web3(process.env.RPC_URL)
+
+
+const uniswapFactoryContract = new web3.eth.Contract(UNISWAP_FACTORY_ABI, UNISWAP_FACTORY_ADDRESS)
+
+const kyberRateContract = new web3.eth.Contract(KYBER_RATE_ABI, KYBER_RATE_ADDRESS)
+
+async function checkPair(args) {
+  const { inputTokenSymbol, inputTokenAddress, outputTokenSymbol, outputTokenAddress, inputAmount } = args
+
+  const exchangeAddress = await uniswapFactoryContract.methods.getExchange(outputTokenAddress).call()
+  const exchangeContract = new web3.eth.Contract(UNISWAP_EXCHANGE_ABI, exchangeAddress)
+
+  const uniswapResult = await exchangeContract.methods.getEthToTokenOutputPrice(inputAmount).call()
+  let kyberResult = await kyberRateContract.methods.getExpectedRate(inputTokenAddress, outputTokenAddress, inputAmount).call()
+
+  console.table([{
+    'Input Token': inputTokenSymbol,
+    'Output Token': outputTokenSymbol,
+    'Input Amount': web3.utils.fromWei(inputAmount, 'Ether'),
+    'Uniswap Return': web3.utils.fromWei(uniswapResult, 'Ether'),
+    'Kyber Expected Rate': web3.utils.fromWei(kyberResult.expectedRate, 'Ether'),
+    'Kyber Min Return': web3.utils.fromWei(kyberResult.worstRate, 'Ether'),
+    'Timestamp': moment().tz('America/Chicago').format(),
+  }])
+}
+
+let priceMonitor
+let monitoringPrice = false
+
+ function monitorPrice() {
+  if(monitoringPrice) {
+    return
+  }
+
+  console.log("Checking prices...")
+  monitoringPrice = true
+
+  try {
+
+    {tokens.map(( async token => {
+
+      await checkPair({
+        inputTokenSymbol: 'ETH',
+        inputTokenAddress: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+        outputTokenSymbol: token.symbol,
+        outputTokenAddress: token.address,
+        inputAmount: web3.utils.toWei('1', 'ETHER')
+      })
+    }))}
+  
+  } catch (error) {
+    console.error(error)
+    monitoringPrice = false
+    clearInterval(priceMonitor)
+    return
+  }
+
+  monitoringPrice = false
+}
+
+// Check markets every n seconds
+const POLLING_INTERVAL = process.env.POLLING_INTERVAL || 3000 // 3 Seconds
+priceMonitor = setInterval(async () => { await monitorPrice() }, POLLING_INTERVAL)
---
+++ src/tokens/index.js
@@ -0,0 +1,4 @@
+exports.tokens = [
+    {symbol : 'DAI', address: '0x6b175474e89094c44da98b954eedeac495271d0f'},
+    {symbol : 'MKR', address: '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2'},
+]
