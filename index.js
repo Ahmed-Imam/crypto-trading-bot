5fd879c8dbe92938f1db37be7f00720fd7a2dd47
ahmed-imam <ahmedimam49@gmail.com>
2021-08-19 23:51:37 +0800

    clean up

--- index.js
+++ index.js
@@ -1,26 +1,11 @@
 require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const http = require('http')
 const Web3 = require('web3')
-const HDWalletProvider = require('@truffle/hdwallet-provider')
 const moment = require('moment-timezone')
-const numeral = require('numeral')
-const _ = require('lodash')
-const axios = require('axios')
 const { UNISWAP_FACTORY_ABI, UNISWAP_FACTORY_ADDRESS, UNISWAP_EXCHANGE_ABI, KYBER_RATE_ABI, KYBER_RATE_ADDRESS }  =  require('./src/exchanges') 
 const { tokens } = require('./src/tokens')
-// SERVER CONFIG
-const PORT = process.env.PORT || 5000
-const app = express();
-const server = http.createServer(app).listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-// WEB3 CONFIG
 const web3 = new Web3(process.env.RPC_URL)
 
-
 const uniswapFactoryContract = new web3.eth.Contract(UNISWAP_FACTORY_ABI, UNISWAP_FACTORY_ADDRESS)
-
 const kyberRateContract = new web3.eth.Contract(KYBER_RATE_ABI, KYBER_RATE_ADDRESS)
 
 async function checkPair(args) {
